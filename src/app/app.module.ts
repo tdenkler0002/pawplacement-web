3aba37f95242cbc44ad4cfccfd9c60b55a0cf874
Tiffany <tiffany.denkler@yahoo.com>
2018-05-12 12:25:23 -0700

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent, NavbarComponent } from './components/index';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
